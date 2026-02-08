94a42151979ce8c7b1e9440bfe41b3ceea703029
Martin Kollar <mato.kollar2004@gmail.com>
2026-02-08 09:01:03 +0100

    Added bibata-cursor theme

--- home/kolby/environment.nix
+++ home/kolby/environment.nix
@@ -3,5 +3,7 @@
 {
   home.sessionVariables = {
     DOTNET_ROOT = "${pkgs.dotnet-sdk}";
+    XCURSOR_THEME = "Bibata-Modern-Classic";
+    XCURSOR_SIZE = "18";
   };
 }
--- home/kolby/fonts.nix
+++ home/kolby/fonts.nix
@@ -7,6 +7,14 @@
     nerd-fonts.ubuntu-mono
   ];
 
+  home.pointerCursor = {
+    name = "Bibata-Modern-Classic";
+    package = pkgs.bibata-cursors;
+    size = 18;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
   fonts.fontconfig = {
     enable = true;
     # antialias = true;
