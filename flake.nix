e4c977ea92f634dfbf5cb0c274d6d31e2adccef8
Martin Kollar <mato.kollar2004@gmail.com>
2026-03-02 09:32:17 +0100

    Changed claude cli to use flake for latest version

--- flake.nix
+++ flake.nix
@@ -1,45 +1,44 @@
 {
   description = "Mato NixOS";
-
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-      nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-      home-manager = {
-        url = "github:nix-community/home-manager/release-25.05";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      quickshell = {
-        url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      caelestia-shell = {
-        url = "github:caelestia-dots/shell";
-        inputs.nixpkgs.follows = "nixpkgs-unstable";
-      };
-      caelestia-cli = {
-        url = "github:caelestia-dots/cli";
-        inputs.nixpkgs.follows = "nixpkgs-unstable";
-      };
-      elegant-grub2-themes = {
-        url = "github:kolby11/elegant-grub2-themes";
-      };
-      kew = {
-        url = "github:kolby11/kew";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      spicetify = {
-        url = "github:Gerg-L/spicetify-nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      yazi.url = "github:sxyazi/yazi";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    caelestia-cli = {
+      url = "github:caelestia-dots/cli";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    elegant-grub2-themes = {
+      url = "github:kolby11/elegant-grub2-themes";
+    };
+    kew = {
+      url = "github:kolby11/kew";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    claude-code.url = "github:sadjow/claude-code-nix";
+    yazi.url = "github:sxyazi/yazi";
   };
-
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, elegant-grub2-themes, ... }@inputs:
-  let 
-      lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
   in
   {
     nixosConfigurations = {
@@ -55,37 +54,27 @@
         };
       };
     };
-
     homeConfigurations = {
-       kolby = home-manager.lib.homeManagerConfiguration {
+      kolby = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
           ./home/kolby
           ({ pkgs, ... }: {
+            nixpkgs.config.allowUnfree = true;
+            nixpkgs.overlays = [ inputs.claude-code.overlays.default ];
             home.packages = [
               (inputs.yazi.packages.${pkgs.system}.default.override {
                 _7zz = pkgs._7zz-rar;
               })
+              pkgs.claude-code
             ];
           })
-          ];
+        ];
         extraSpecialArgs = {
           inherit pkgs-unstable;
           inherit inputs;
           context = self;
           runtimeRoot = "/home/kolby/.config";
-          hm = home-manager.lib.hm;
-        };
-      };
-      mato = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home/mato ];
-        extraSpecialArgs = {
-          inherit pkgs-unstable;
-          inherit inputs;
-          context = self;
-          runtimeRoot = "/home/mato/.config";
           hm = home-manager.lib.hm;
         };
       };
